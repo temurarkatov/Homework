3b271999b853fc3e179ea76c2315cb405592f137
Тимур <@DESKTOP-LK5N1TP>
2025-09-29 23:33:43 +0300

    BinaryTree day1

---
+++ Recursion/main.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+void elevator(int floor);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Ââåäèòå íîìåð ýòàæà: "; cin >> n;
+	elevator(n);
+	//cout << "Recusion";
+	//main();
+}
+void elevator(int floor)
+{
+	if (floor == 0)
+	{
+		cout << "Âû â ïîäâàëå" << endl;
+		return;
+	}
+	cout << "Âû íà " << floor << " ýòàæå" << endl;
+	elevator(floor - 1);
+	cout << "Âû íà " << floor << " ýòàæå" << endl;
+}
