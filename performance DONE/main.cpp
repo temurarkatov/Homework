f992e042c8b4228a5f6da9476f494138901d9a53
Тимур <@DESKTOP-LK5N1TP>
2025-09-29 23:47:23 +0300

    performance DONE

---
+++ performance DONE/main.cpp
@@ -0,0 +1,325 @@
+﻿#include<iostream>
+#include<time.h>
+#include<algorithm>
+#include<math.h>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+//#define MAX(a,b)a>b?a:b
+
+//#define DEBUG
+
+class Tree
+{
+protected:
+	class Element
+	{
+		int Data;
+		Element* pLeft;
+		Element* pRight;
+	public:
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) :
+			Data(Data), pLeft(pLeft), pRight(pRight)
+		{
+#ifdef DEBUG
+			cout << "EConstructor:\t" << this << endl;
+#endif // DEBUG
+		}
+		~Element()
+		{
+#ifdef DEBUG
+			cout << "EDestructor:\t" << this << endl;
+#endif // DEBUG
+		}
+		friend class Tree;
+		friend class UniqueTree;
+	}*Root;
+public:
+	Element* getRoot()const
+	{
+		return Root;
+	}
+	Tree() :Root(nullptr)
+	{
+		cout << "TConstructor:\t" << this << endl;
+	}
+	Tree(const std::initializer_list<int>& il) :Tree()
+	{
+		for (int const* it = il.begin(); it != il.end(); ++it)
+			insert(*it, Root);
+		cout << "ILConstructor:\t" << this << endl;
+	}
+	~Tree()
+	{
+		clear();
+		cout << "TDestructor:\t" << this << endl;
+	}
+	void clear()
+	{
+		clear(Root);
+	}
+	void insert(int Data)
+	{
+		insert(Data, Root);
+	}
+	void erase(int Data)
+	{
+		erase(Data, Root);
+	}
+	int minValue()const
+	{
+		return minValue(Root);
+	}
+	int maxValue()const
+	{
+		return maxValue(Root);
+	}
+	int sum()const
+	{
+		return sum(Root);
+	}
+	int count()const
+	{
+		return count(Root);
+	}
+	double avg()const
+	{
+		return (double)sum(Root) / count(Root);
+	}
+	int depth()const
+	{
+		return depth(Root);
+	}
+	void print()const
+	{
+		print(Root);
+		cout << endl;
+	}
+private:
+	void clear(Element*& Root)
+	{
+		if (Root == nullptr)return;
+		clear(Root->pLeft);
+		clear(Root->pRight);
+		delete Root;
+		Root = nullptr;
+	}
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr) this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+	void erase(int Data, Element*& Root)
+	{
+		if (Root == nullptr)return;
+		erase(Data, Root->pLeft);
+		erase(Data, Root->pRight);
+		if (Data == Root->Data)
+		{
+			if (Root->pLeft == Root->pRight)
+			{
+				delete Root;
+				Root = nullptr;
+			}
+			else
+			{
+				//Äëÿ òîãî ÷òîáû äåðåâî áàëàíñèðîâàëîñü ïðè óäàëåíèè ýëåìåíòîâ,
+				//ïåðåä óäàëåíèåì åãî íóæíî âçâåñèòü:
+				if (count(Root->pLeft) > count(Root->pRight))
+				{
+					//è åñëè ëåâàÿ âåòêà òÿæåëåå ÷åì ïðàâàÿ, òî áåðåì èç íåå ìàêñèìàëüíîå çíà÷åíèå,
+					//ïîòîìó ÷òî îíî áëèæå âñåãî ê óäàëÿåìîìó çíà÷åíèþ:
+					Root->Data = maxValue(Root->pLeft);
+					erase(maxValue(Root->pLeft), Root->pLeft);
+				}
+				else
+				{
+					//â ïðîòèâíîì ñëó÷àå áåðåì ìèíèìàëüíîå çíà÷åíèå èç ïðàâîé âåòêè,
+					//ïîòîìó ÷òî îíî áëèæå âñåãî ê óäàâëÿåìîìó çíà÷åíèþ:
+					Root->Data = minValue(Root->pRight);
+					erase(minValue(Root->pRight), Root->pRight);
+				}
+			}
+		}
+	}
+	int minValue(Element* Root)const
+	{
+		return Root == nullptr ? INT_MIN : Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+		/*if (Root->pLeft == nullptr)return Root->Data;
+		else return minValue(Root->pLeft);*/
+	}
+	int maxValue(Element* Root)const
+	{
+		return !Root ? INT_MIN : Root->pRight ? maxValue(Root->pRight) : Root->Data;
+		/*if (Root->pRight == nullptr)return Root->Data;
+		else return maxValue(Root->pRight);*/
+	}
+	int count(Element* Root)const
+	{
+		return !Root ? 0 : count(Root->pLeft) + count(Root->pRight) + 1;
+		/*if (Root == nullptr)return 0;
+		else return count(Root->pLeft) + count(Root->pRight) + 1;*/
+	}
+	int sum(Element* Root)const
+	{
+		return Root == nullptr ? 0 : sum(Root->pLeft) + sum(Root->pRight) + Root->Data;
+	}
+
+	int depth(Element* Root)const
+	{
+		
+			return Root == nullptr ? 0 : std::max(depth(Root->pLeft) + 1, depth(Root->pRight) + 1);
+
+		/*if (Root == nullptr)return 0;
+		int l_depth = depth(Root->pLeft)+1;
+		int r_depth = depth(Root->pRight)+1;
+		return l_depth < r_depth ? r_depth : l_depth;*/
+
+		/*return
+			Root == nullptr ? 0 :
+			depth(Root->pLeft) + 1 > depth(Root->pRight) + 1 ?
+			depth(Root->pLeft) + 1 :
+			depth(Root->pRight) + 1;
+			depth(Root->pRight) + 1;*/
+
+	}
+	void print(Element* Root)const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << tab;
+		print(Root->pRight);
+	}
+};
+class UniqueTree :public Tree
+{
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr) this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else if (Data > Root->Data)
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+public:
+	void insert(int Data)
+	{
+		insert(Data, Root);
+	}
+};
+
+template<typename T>void measure_performance(const char message[], T(Tree::* function)()const, const Tree& tree)
+{
+	//int (*function)() - óêàçàòåëü íà ôóíêöèþ, êîòîðàÿ íè÷åãî íå ïðèíèìàåò, è âîçâðàùàåò çíà÷åíèå òèïà 'int'.
+	clock_t start = clock();
+	T result = (tree.*function)();
+	clock_t end = clock();
+	cout << message << result << ", âû÷èñëåíî çà " << double(end - start) / CLOCKS_PER_SEC << " ñåêóíä\n";
+}
+
+//#define BASE_CHECK
+//#define ERASE_CHECK
+#define PERFORMANCE_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef BASE_CHECK
+	int n;
+	cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ: "; cin >> n;
+	Tree tree;
+	cout << "Ìèíèìàëüíîå çíà÷åíèå â äåðåâå: " << tree.minValue() << endl;
+	cout << "Ìàêñèìàëüíîå çíà÷åíèå â äåðåâå: " << tree.maxValue() << endl;
+	for (int i = 0; i < n; i++)
+	{
+		tree.insert(rand() % 100);
+	}
+	tree.print();
+	cout << endl;
+	cout << "Ìèíèìàëüíîå çíà÷åíèå â äåðåâå: " << tree.minValue() << endl;
+	cout << "Ìàêñèìàëüíîå çíà÷åíèå â äåðåâå: " << tree.maxValue() << endl;
+	cout << "Êîëè÷åñòâî ýëåìåíòîâ äåðåâà: " << tree.count() << endl;
+	cout << "Ñóììà ýëåìåíòîâ äåðåâà: " << tree.sum() << endl;
+	cout << "Ñðåäíåå-àðèôìåòè÷åñêîå ýëåìåíòîâ äåðåâà: " << tree.avg() << endl;
+
+	UniqueTree u_tree;
+	for (int i = 0; i < n; i++)
+	{
+		u_tree.insert(rand() % 100);
+	}
+	u_tree.print();
+	cout << "Ìèíèìàëüíîå çíà÷åíèå â äåðåâå: " << u_tree.minValue() << endl;
+	cout << "Ìàêñèìàëüíîå çíà÷åíèå â äåðåâå: " << u_tree.maxValue() << endl;
+	cout << "Êîëè÷åñòâî ýëåìåíòîâ äåðåâà: " << u_tree.count() << endl;
+	cout << "Ñóììà ýëåìåíòîâ äåðåâà: " << u_tree.sum() << endl;
+	cout << "Ñðåäíåå-àðèôìåòè÷åñêîå ýëåìåíòîâ äåðåâà: " << u_tree.avg() << endl;
+#endif // BASE_CHECK
+
+#ifdef ERASE_CHECK
+	Tree tree =
+	{
+					50,
+
+			25,				75,
+
+		16,		32,		58,		85,	91,	98
+	};
+	tree.print();
+
+	int value;
+	//cout << "Ââåäèòå óäàëÿåìîå çíà÷åíèå: "; cin >> value;
+	/*tree.erase(25);
+	tree.erase(32);
+	tree.erase(50);
+	tree.erase(75);*/
+	tree.print();
+	cout << "Ãðóáèíà äåðåâà: " << tree.depth() << endl;
+#endif // ERASE_CHECK
+
+#ifdef PERFORMANCE_CHECK
+	int n;
+	cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ: "; cin >> n;
+	Tree tree;
+	for (int i = 0; i < n; i++)
+	{
+		tree.insert(rand() % 100);
+	}
+	/*tree.print();
+	cout << "Ìèíèìàëüíîå çíà÷åíèå â äåðåâå: " << tree.minValue() << endl;
+	cout << "Ìàêñèìàëüíîå çíà÷åíèå â äåðåâå: " << tree.maxValue() << endl;
+	cout << "Êîëè÷åñòâî ýëåìåíòîâ äåðåâà: " << tree.count() << endl;
+	cout << "Ñóììà ýëåìåíòîâ äåðåâà: " << tree.sum() << endl;
+	cout << "Ñðåäíåå-àðèôìåòè÷åñêîå ýëåìåíòîâ äåðåâà: " << tree.avg() << endl;
+	cout << "Ãëóáèíà äåðåâà: " << tree.depth() << endl;*/
+
+	measure_performance("Ìèíèìàëüíîå çíà÷åíèå â äåðåâå: ", &Tree::minValue, tree);
+	measure_performance("Ìàêñèìàëüíîå çíà÷åíèå â äåðåâå: ", &Tree::maxValue, tree);
+	measure_performance("Ñóììà ýëåìíòîâ äåðåâà: ", &Tree::sum, tree);
+	measure_performance("Êîëè÷åñòâî ýëåìíòîâ äåðåâà: ", &Tree::count, tree);
+	measure_performance("Ñðåäíåå-àðèôìåòè÷åñêî ýëåìíòîâ äåðåâà: ", &Tree::avg, tree);
+	measure_performance("Ãëóáèíà äåðåâà: ", &Tree::depth, tree);
+#endif // PERFORMANCE_CHECK
+
+
+}
